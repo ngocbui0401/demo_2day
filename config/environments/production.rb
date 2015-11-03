1e037538b9986380d9e5f18f86310c6adfe007b5
ngocnep0401 <bui.thi.ngoc@framgia.com>
2015-11-03 10:43:37 +0700

    fixed email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://demo-app-daodat.herokuapp.com'
+  host = 'https://demo-app-ngocbuithi.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address:         'smtp.gmail.com',
